005f9b5805f0f9706e00b8391ddb774aa621ca03
Michelle <vand0432@algonquinlive.com>
2016-03-15 18:58:05 -0400

    List appending lis.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $input = $('#to-do-input');
+var $list = $('.list');
+
+
+$('#to-do-list').on('submit', function (e) {
+  var $li = $('<li>');
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+});
