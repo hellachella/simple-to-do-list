1014d8ff64a561228e266acd5227800bbff0ae25
Michelle <vand0432@algonquinlive.com>
2016-03-15 19:58:41 -0400

    line through

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@ var $list = $('.list');
 
 $('#to-do-list').on('submit', function (e) {
   var $li = $('<li>');
-
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+});
 
-  $('ul').on('click', 'li', function (f) {
+$('ul').on('click', 'li', function (e) {
   $(this).toggleClass('strike');
 });
 
-});
 
 
