22d9e953200153741893898c161045a4ee520b5f
Michelle <vand0432@algonquinlive.com>
2016-03-15 19:05:29 -0400

    Not working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 var $input = $('#to-do-input');
 var $list = $('.list');
 
-
 $('#to-do-list').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+
+  $('ul').on('click', 'li', function (f) {
+  $(this).toggleClass('strike');
 });
+
+});
+
+
